e7c77a87b82e2b793cd5a0cfc62eb9cb0c7bcc80
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2024-02-22 01:49:59 +0530

    Fix the size of Floating Action Buttons in SearchScreen

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/views/MultiFloatingActionButton.kt
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/views/MultiFloatingActionButton.kt
@@ -9,12 +9,14 @@ import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
@@ -51,7 +53,9 @@ fun FabItem(
     FloatingActionButton(
         onClick = {
             onFabClick(fabButton.fabType)
-        }
+        },
+        modifier = Modifier
+            .size(48.dp)
     ) {
         Icon(
             painter = painterResource(id = fabButton.iconRes),
@@ -73,13 +77,15 @@ fun MultiFloatingActionButton(
         else
             0f, label = "mainFabRotation"
     )
-    Column {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         AnimatedVisibility(
             visible = fabButtonState.isExpanded(),
             enter = fadeIn() + expandVertically(),
             exit = fadeOut() + shrinkVertically()
         ) {
-            Column{
+            Column {
                 fabButtons.forEach {
                     FabItem(
                         fabButton = it,
